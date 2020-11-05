c2750677ff67cde8b28e4b784325545c6d5455a4
David Kanekanian <dkanekanian1@gmail.com>
2020-11-05 18:52:25 +0000

    Add main menu scene

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { updateCommaList } from "typescript";
 import { ex } from "./imports.js";
+import { MainMenuScene } from "./scenes/mainMenu.js";
 
 const game = new ex.Engine({
     antialiasing: false,
     backgroundColor: ex.Color.Azure
 });
 
+// Add scenes here.
+game.addScene("mainMenu", new MainMenuScene(game));
+
+// Start the actual game.
+game.goToScene("mainMenu");
 game.start();
---
+++ src/scenes/mainMenu.ts
@@ -0,0 +1,7 @@
+import { ex } from "../imports.js";
+
+export class MainMenuScene extends ex.Scene {
+    public onActivate(oldScene: ex.Scene, newScene: ex.Scene) {
+        console.log("started main menu");
+    }
+}
