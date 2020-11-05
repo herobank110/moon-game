4e8ec9ce835cdbcbcd1da6c5def65b791df04112
David Kanekanian <dkanekanian1@gmail.com>
2020-11-05 18:40:42 +0000

    Add excalibur blank game full screen

---
+++ src/imports.ts
@@ -0,0 +1,2 @@
+// @ts-ignore
+export var ex: typeof import("excalibur") = window["ex"];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello, world');
+import { updateCommaList } from "typescript";
+import { ex } from "./imports.js";
+
+const game = new ex.Engine({
+    antialiasing: false,
+    backgroundColor: ex.Color.Azure
+});
+
+game.start();
