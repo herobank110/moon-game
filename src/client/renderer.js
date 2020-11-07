a878b238b4883dd0b41b8702b96d6bf03e624944
David Kanekanian <dkanekanian1@gmail.com>
2020-11-07 18:22:43 +0000

    Add animated sprite sheet characters

--- src/client/renderer.js
+++ src/client/renderer.js
@@ -1,6 +1,7 @@
 import { Renderer } from 'lance-gg';
 import { resources, loader } from './resources';
 import { Color, Engine, Scene } from 'excalibur';
+import { MenuScene, OvergroundScene } from './scenes';
 
 /**
  * Only exists on clients. Maps lance-gg world to excalibur for drawing.
@@ -17,13 +18,13 @@ export class MoonRenderer extends Renderer {
         this.game.start(loader);
     }
 
-
     initScenes() {
         // Add more scenes below.
-        this.game.addScene('menu', new Scene());
+        this.game.addScene('menu', new MenuScene(this.game));
+        this.game.addScene('overground', new OvergroundScene(this.game));
 
         // Set the starting scene.
-        this.game.goToScene('menu');
+        this.game.goToScene('overground');
     }
 
     draw(t, dt) {
---
+++ src/client/scenes.js
@@ -0,0 +1,54 @@
+import { Scene, Actor, SpriteSheet } from 'excalibur';
+import { resources } from './resources';
+
+export class MenuScene extends Scene {
+    // TODO
+}
+
+export class OvergroundScene extends Scene {
+    onInitialize(engine) {
+        const spr = resources.spaceBg.asSprite();
+        spr.scale.setTo(0.1, 0.1);
+        this.makeSpaceBg(spr, -100);
+        this.makeSpaceBg(spr, 134);
+        this.makeSpaceBg(spr, 368);
+
+        this.makeP1(engine);
+        this.makeP2(engine);
+
+        this.camera.pos.setTo(0, -20);
+        this.camera.zoom(5);
+    }
+
+    makeP1(engine) {
+        const pl = new Actor({ x: 0, y: 0, width: 100, height: 100 });
+        const spr = new SpriteSheet(resources.character1, 4, 6, 16, 16);
+        pl.addDrawing('idle', spr.getAnimationBetween(engine, 0, 6, 100));
+        pl.addDrawing('walk', spr.getAnimationBetween(engine, 6, 12, 100));
+        pl.setDrawing('idle');
+        this.add(pl);
+
+        this.p1 = pl;
+        return this.p1;
+    }
+
+    makeP2(engine) {
+        const pl = new Actor({ x: 10, y: 0, width: 100, height: 100 });
+        const spr = new SpriteSheet(resources.character2, 4, 6, 16, 16);
+        pl.addDrawing('idle', spr.getAnimationBetween(engine, 0, 6, 100));
+        pl.addDrawing('walk', spr.getAnimationBetween(engine, 6, 12, 100));
+        pl.setDrawing('idle');
+        this.add(pl);
+
+        this.p2 = pl;
+        return this.p2;
+    }
+
+    makeSpaceBg(spr, offset) {
+        const bg = new Actor();
+        bg.addDrawing(spr);
+        bg.pos.setTo(offset, -50);
+        this.add(bg);
+        return bg;
+    }
+}
